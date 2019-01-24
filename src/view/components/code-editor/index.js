9a14bc6c7baba169edffe66a7ea87f8dba8cdf8b
Ozan Kaşıkçı <kasikciozan@yandex.com>
2019-01-24 22:27:44 +0100

    remove toJS

--- src/view/components/code-editor/index.js
+++ src/view/components/code-editor/index.js
@@ -1,16 +1,13 @@
 import './styles.scss';
-
-
 import { algorithmStore, languageStore } from '../../../stores';
 import CodeSnippet from '../code-snippet';
-import { toJS } from 'mobx';
 
 export default function () {
   return {
     view: () => {
       return (
         <div className="uk-child-width-1-2@m" uk-grid>
-          { toJS(languageStore.languages).map((language, i) => {
+          { languageStore.languages.map((language, i) => {
             const id = `editor-${language.extension}`;
             return (<CodeSnippet languageName={language.name} selector={id} i={i}/>) ;
           }) }
