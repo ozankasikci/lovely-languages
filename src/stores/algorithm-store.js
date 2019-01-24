acd12e421656f84f821fc9c354a4af0158d29563
Ozan Kaşıkçı <kasikciozan@yandex.com>
2019-01-24 17:30:21 +0100

    remove methods

--- src/stores/algorithm-store.js
+++ src/stores/algorithm-store.js
@@ -4,26 +4,6 @@ import algorithms from './data/algorithm';
 class AlgorithmStore {
   @observable algorithms = algorithms;
 
-  constructor() {
-    autorun(() => console.log(this.report));
-  }
-
-  @action addBike(data) {
-    console.log(data);
-    this.bikes.push({
-      mediaId: data.mediaId,
-      displayName: data.displayName,
-      new: false,
-    });
-  }
-
-  @action addBikes(data) {
-    console.log('adding bikes');
-    data.products.forEach((bike) => {
-      console.log('adding bike', bike.displayName);
-      this.addBike(bike);
-    });
-  }
 }
 
 export default AlgorithmStore;
--- src/stores/language-store.js
+++ src/stores/language-store.js
@@ -3,28 +3,7 @@ import languages from './data/language';
 
 class LanguageStore {
   @observable languages = languages;
-  @observable pendingRequests = 0;
-
-  constructor() {
-    autorun(() => console.log(this.report));
-  }
-
-  @action addBike(data) {
-    console.log(data);
-    this.bikes.push({
-      mediaId: data.mediaId,
-      displayName: data.displayName,
-      new: false,
-    });
-  }
-
-  @action addBikes(data) {
-    console.log('adding bikes');
-    data.products.forEach((bike) => {
-      console.log('adding bike', bike.displayName);
-      this.addBike(bike);
-    });
-  }
+  
 }
 
 export { languages };
