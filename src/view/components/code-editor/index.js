e38e877030d2edca8ac668efeeca48bea4a98aad
Oğuzhan Aslan <aslan@oguzhan.in>
2019-02-02 20:58:36 +0300

    feat: css  improvement

--- src/view/components/code-editor/index.js
+++ src/view/components/code-editor/index.js
@@ -6,11 +6,19 @@ export default function () {
   return {
     view: () => {
       return (
-        <div className="uk-child-width-1-2@m" uk-grid>
-          { languageStore.languages.map((language, i) => {
-            const id = `editor-${language.extension}`;
-            return (<CodeSnippet languageName={language.name} selector={id} i={i}/>) ;
-          }) }
+        <div>
+          <div className="lang-list">
+            { languageStore.languages.map((language, i) => {
+              return (<a class="uk-category-btn uk-button uk-button-default" href="">{language.name}</a>) ;
+            }) }
+
+          </div>
+          <div className="uk-child-width-1-2@m" uk-grid>
+            { languageStore.languages.map((language, i) => {
+              const id = `editor-${language.extension}`;
+              return (<CodeSnippet languageName={language.name} selector={id} i={i}/>) ;
+            }) }
+          </div>
         </div>
       );
     },
