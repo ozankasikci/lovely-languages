614bd25686e91518f689042bcc872c1bda26ccba
Ozan Kaşıkçı <kasikciozan@yandex.com>
2019-02-13 01:28:44 +0300

    remove splash screen

--- src/view/components/about/index.js
+++ src/view/components/about/index.js
@@ -10,7 +10,7 @@ export default function () {
           They all have their strong and weak sides. This project aims to reveal all these different pros and cons.
           And most importantly to admire the beauty of all the programming languages.
         </p>
-        <p>Feel free to contribute to the project, add new algorithms and new programming languages!</p>
+        <p>Feel free to contribute to the project, add new algorithms and new programming languages, spread the knowledge!</p>
       </div>);
     },
   };
--- src/view/routes.js
+++ src/view/routes.js
@@ -3,37 +3,12 @@ import PageLayout from './components/page-layout';
 // Individual pages
 import IndexPage from './pages/landing-page';
 import AboutPage from './pages/about';
-import Splash from './components/splash-loader/index';
-import MaintenancePage from './components/maintenance-layout/index';
 
 const $root = document.body.querySelector('#root');
 
 const Routes = {
-  '/splash': {
-    render() {
-      return m(PageLayout, m(Splash));
-    },
-  },
   '/index': {
-    // onmatch() {
-    //   // Show Loader until the promise has been resolved or rejected.
-    //   m.render($root, m(PageLayout, m(Splash)));
-    //
-    //   return new Promise((resolve/*, reject*/) => {
-    //     //Fetch all necessary data here
-    //     setTimeout(function() {
-    //       resolve();
-    //     }, 500);
-    //   }).catch((/* e */) => {
-    //     // In case of server error we can show the maintenance page.
-    //     return MaintenancePage;
-    //   });
-    // },
-    render(vnode) {
-      // if (typeof vnode.tag === 'function') {
-      //   //If onmatch returns a component or a promise that resolves to a component, comes here.
-      //   return vnode;
-      // }
+    render() {
       return m(PageLayout, m(IndexPage));
     },
   },
