edf02b3d99de933dbe51a1bedd445d0a0ea236c6
Ozan Kaşıkçı <kasikciozan@yandex.com>
2019-02-17 23:50:20 +0100

    src: components: code-editor: clean up

--- src/view/components/code-editor/index.js
+++ src/view/components/code-editor/index.js
@@ -7,6 +7,7 @@ export default function () {
     view: () => {
       const allLanguagesActive = languageStore.selectedAllLanguages ? 'active' : '';
 
+      // add default "all" button
       const languageButtons = [
         <a
           className={`uk-category-btn uk-button uk-button-default ${allLanguagesActive}`}
@@ -18,10 +19,15 @@ export default function () {
         >All</a>,
       ];
 
+      // add other language buttons
       const buttons = languageButtons.concat(
         languageStore.languages.map((language, i) => {
+          const active = `${languageStore.selectedAllLanguages
+            ? 'active'
+            : languageStore.selectedLanguages.includes(language.name) && 'active'}`;
+
           return (<a
-            className={`uk-category-btn uk-button uk-button-default ${languageStore.selectedAllLanguages ? 'active' : languageStore.selectedLanguages.includes(language.name) && 'active'}`}
+            className={`uk-category-btn uk-button uk-button-default ${active}`}
             href="#"
             onclick={e => {
               e.preventDefault();
@@ -43,9 +49,9 @@ export default function () {
           <div className="lang-list">{buttons}</div>
           <div className="uk-child-width-1-2@m" uk-grid>
             { languageStore.languages.map((language, i) => {
-              const className = languageStore.selectedLanguages.includes(language.name) || allLanguagesActive ? '' : 'uk-hidden';
+              const classNames = languageStore.selectedLanguages.includes(language.name) || allLanguagesActive ? '' : 'uk-hidden';
               const id = `editor-${language.extension}`;
-              return (<CodeSnippet classNames={className} languageName={language.name} selector={id} i={i}/>);
+              return (<CodeSnippet classNames={classNames} languageName={language.name} selector={id} i={i}/>);
             })}
           </div>
         </div>
@@ -53,9 +59,6 @@ export default function () {
     },
     oncreate: () => {
       algorithmStore.createEditors();
-      algorithmStore.setEditorValues();
-    },
-    onremove: () => {
       algorithmStore.setEditorValues();
     },
   };
